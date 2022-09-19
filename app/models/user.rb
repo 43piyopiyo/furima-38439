8974f2ac6414c2b8860ba8a1ad1149895fab19fc
43piyopiyo <spitz.green.43@gmail.com>
2022-09-19 17:47:58 +0900

    Userモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   validates :nickname,        presence: true
   validates :email,           presence: true
-  validates :password,       presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :ruby_last_name,  presence: true
-  validates :ruby_first_name, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password,        presence: true, format: { with: VALID_PASSWORD_REGEX }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u} do
+    validates :ruby_last_name
+    validates :ruby_first_name
+  end
   validates :birthday,        presence: true
 end
