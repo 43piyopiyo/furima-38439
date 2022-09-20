066c34bfe818c3a3d0805689977c2868fa8dec1f
43piyopiyo <spitz.green.43@gmail.com>
2022-09-20 14:38:43 +0900

    Itemモデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :delivery_free, :prefecture, :shipping_day
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_free
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   validates :name,                    presence: true
   validates :detail,                  presence: true
