296a8fa3c63bca37384d35809a419222ebecc705
43piyopiyo <spitz.green.43@gmail.com>
2022-09-20 12:57:57 +0900

    Itemモデル、Itemsテーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  # has_one :order
+
+  validates :name,                    presence: true
+  validates :detail,                  presence: true
+  validates :category_id,             presence: true
+  validates :condition_id,            presence: true
+  validates :delivery_free_id,        presence: true
+  validates :prefecture_id,           presence: true
+  validates :shipping_day_id,         presence: true
+  validates :price,                   presence: true
+  validates :user,                    presence: true
+
+end
