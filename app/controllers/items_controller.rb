6c1656a5df41c348612b4badeb353a1703baf39e
43piyopiyo <spitz.green.43@gmail.com>
2022-09-20 13:17:11 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :detail, :category_id, :condition_id, :delivery_free_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one :order
+  has_one_attached :image
+
 
   validates :name,                    presence: true
   validates :detail,                  presence: true
