50fec727eb9aa9bacf727e997a6739b59171a54e
43piyopiyo <spitz.green.43@gmail.com>
2022-09-28 10:49:53 +0900

    エラーメッセージ重複の解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,20 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :image,                   presence: true
-  validates :name,                    presence: true
-  validates :detail,                  presence: true
-  validates :category_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id,            presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_free_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                   presence: true, numericality: { greater_than_or_equal_to: 300, message: 'must be greater_than_or_equal_to 300' }
-  validates :price,                   presence: true, numericality: { less_than_or_equal_to: 9999999, message: 'must be less_than_or_equal_to 9999999' }
-  validates :price,                   presence: true, numericality: { only_integer: true, message: 'should be half-width numbers' }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :detail
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :delivery_free_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :price
+  end
+
+  validates :price, allow_blank: true, numericality: { greater_than_or_equal_to: 300, message: 'should be greater_than_or_equal_to 300' }
+  validates :price, allow_blank: true, numericality: { less_than_or_equal_to: 9999999, message: 'should be less_than_or_equal_to 9999999' }
+  validates :price, allow_blank: true, numericality: { only_integer: true, message: 'should be half-width numbers' }
+
 end
